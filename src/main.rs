1b3c63d2e35ba3dfb31fabc3a65af02cfdd9326a
973Karljoy <karljoyeux@gmail.com>
2025-01-15 03:43:17 +0100

    add timer to the app

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
     
     App::new()
@@ -22,11 +25,6 @@ fn add_people(mut commands: Commands) {
 fn hello_world() {
     println!("hello world!");
 }
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
     for mut name in &mut query {
@@ -37,13 +35,24 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people,greet_people))
-        .chain());
+        fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
+
 }
